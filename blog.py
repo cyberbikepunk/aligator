380f6bce6d84e256a080e398b0034fc95ae3278a
cyberbikepunk <loic@cyberpunk.bike>
2015-09-03 12:41:21 +0200

    Integrated bootstrap.

--- blog.py
+++ blog.py
@@ -1,17 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from wtforms import StringField, SubmitField, Form
+from wtforms.validators import DataRequired
+from flask import Markup
+from markdown import markdown
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
-@app.route('/<user>/')
-def user(user):
-    return 'Hello %s!' % user
+@app.template_filter('markdown')
+def markdown_filter(data):
+    return Markup(markdown(data))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    # if form.validate():
+    #     name = form.name.data
+    #     form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
---
+++ posts/test.md
@@ -0,0 +1,14 @@
+Test
+=====
+
+foo
+---
+__hello world__  *this is something in italic*
+
+##bar
+1. bla
+2. bla
+
+```
+this is a code snippet
+```
